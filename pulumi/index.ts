dace7d486bad65b403352d390b8e594dacdabbbf
Mathis Kretz <mathis@bespinian.io>
2021-07-25 10:31:51 +0200

    Use awesome image in pulumi example

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,10 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
 
-const image = new docker.RemoteImage("ubuntu", {
-  name: "ubuntu:precise",
+const image = new docker.RemoteImage("awesome", {
+  name: "bespinian/awesome-image:1.0.0",
 });
 
-const container = new docker.Container("ubuntu", {
+const container = new docker.Container("awesome", {
   image: image.latest,
+  ports: [{ external: 8080, internal: 8080 }],
+  envs: ["APP_TITLE=awesome"],
 });
