21d83f7636c6410a75938568058a1f2b968ff39d
Mathis Kretz <mathis@bespinian.io>
2021-07-25 11:50:19 +0200

    Add first component module

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,12 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as docker from "@pulumi/docker";
+import * as virt from "./components/virtualization";
 
-const image = new docker.RemoteImage("awesome", {
-  name: "bespinian/awesome-image:1.0.0",
-});
-
-const container = new docker.Container("awesome", {
-  image: image.latest,
-  ports: [{ external: 8080, internal: 8080 }],
-  envs: ["APP_TITLE=awesome"],
-});
+const virtualization = new virt.Virtualization(
+  "virtualization",
+  virt.VirtualizationSize.Large,
+  {}
+);
