6c9c73c1042835ae4796d51a15e75462a91b1997
Mathis Kretz <mathis@bespinian.io>
2021-07-26 16:26:41 +0200

    Nest components correctly

--- pulumi/components/gitlab.ts
+++ pulumi/components/gitlab.ts
@@ -7,25 +7,36 @@ enum GitlabSize {
 }
 
 interface GitlabArgs {
-  virtUrl: pulumi.Input<string>;
-  size: pulumi.Input<GitlabSize>;
+  readonly virtUrl: pulumi.Input<string>;
+  readonly size: pulumi.Input<GitlabSize>;
 }
 
 class Gitlab extends pulumi.ComponentResource {
+  image: docker.RemoteImage;
+  servers: docker.Container[];
   constructor(name: string, args: GitlabArgs, opts: pulumi.ResourceOptions) {
-    super("components:Gitlab", name, {}, opts);
+    super("platform:components:Gitlab", name, args, opts, false);
 
     let count = args.size == GitlabSize.Large ? 3 : 1;
-    console.log(pulumi.interpolate`Virtualization URL: ${args.virtUrl}`);
-    let image = new docker.RemoteImage("awesome-gitlab", {
-      name: "bespinian/awesome-image:1.0.0",
-    });
+    this.image = new docker.RemoteImage(
+      "awesome-gitlab",
+      {
+        name: "bespinian/awesome-image:1.0.0",
+      },
+      { parent: this }
+    );
+    this.servers = [];
     for (var i = 0; i < count; i++) {
-      let server = new docker.Container(`${name}-${i}` + i, {
-        image: image.latest,
-        ports: [{ external: 9080 + i, internal: 8080 }],
-        envs: [`APP_TITLE=${name}-${i}`],
-      });
+      let server = new docker.Container(
+        `${name}-${i}` + i,
+        {
+          image: this.image.latest,
+          ports: [{ external: 9080 + i, internal: 8080 }],
+          envs: [`APP_TITLE=${name}-${i}`],
+        },
+        { parent: this }
+      );
+      this.servers.push(server);
     }
 
     this.registerOutputs();
--- pulumi/components/virtualization.ts
+++ pulumi/components/virtualization.ts
@@ -8,28 +8,39 @@ enum VirtualizationSize {
 
 class Virtualization extends pulumi.ComponentResource {
   public readonly url: pulumi.Output<string>;
+  image: docker.RemoteImage;
+  hosts: docker.Container[];
 
   constructor(
     name: string,
     size: VirtualizationSize,
     opts: pulumi.ResourceOptions
   ) {
-    super("components:Virtualization", name, {}, opts);
+    super("platform:components:Virtualization", name, {}, opts);
 
     let count = size == VirtualizationSize.Large ? 3 : 1;
 
-    let image = new docker.RemoteImage("awesome-virtualization", {
-      name: "bespinian/awesome-image:1.0.0",
-    });
+    this.image = new docker.RemoteImage(
+      "awesome-virtualization",
+      {
+        name: "bespinian/awesome-image:1.0.0",
+      },
+      { parent: this }
+    );
 
     let urls = [];
-
+    this.hosts = [];
     for (var i = 0; i < count; i++) {
-      let host = new docker.Container(`${name}-${i}` + i, {
-        image: image.latest,
-        ports: [{ external: 8080 + i, internal: 8080 }],
-        envs: [`APP_TITLE=${name}-${i}`],
-      });
+      let host = new docker.Container(
+        `${name}-${i}` + i,
+        {
+          image: this.image.latest,
+          ports: [{ external: 8080 + i, internal: 8080 }],
+          envs: [`APP_TITLE=${name}-${i}`],
+        },
+        { parent: this }
+      );
+      this.hosts.push(host);
       urls.push(host.id);
     }
 
